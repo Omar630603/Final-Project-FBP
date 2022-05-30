cc3ebd0796c98fafa6aebe03e7d1a2f8b4583a05
widiarsaf <wretasafitri33@gmail.com>
2022-05-30 15:41:49 +0700

    add why choose us section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../Index.css";
 import Jumbotron from "../Components/Jumbotron/Jumbotron";
+import WhyChooseUs from "../Components/WhyChooseUs/WhyChooseUs";
 
 const Home=() => {
 	return (
 		<div className="home-container">
-				<Jumbotron/>
+			<Jumbotron />
+			<WhyChooseUs/>
 		</div>
 	);
 };
