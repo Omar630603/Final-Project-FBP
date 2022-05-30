d15fa629b58141299dc43d588f36c81807539273
hanumaisyaah <hanumaisyaah@gmail.com>
2022-05-30 17:49:15 +0700

    edit About Us

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -4,32 +4,24 @@ import React from "react";
 // Css
 import "./AboutUs.css";
 
+import Logo from "../../Assets/Logo.png";
 
-const AboutUs=() => {
-	return (
-		<div className="aboutus-container">
-			<div className="aboutus-text-container">
-				<div className="text-group">
-					<h3>About Us</h3>
-				</div>
-			</div>
-			<div className="aboutus-card-container">
-				<div className="card-container">
-					<div className="image-container">
-						<img src={Logo} alt="" className="image-icon" />
-					</div>
-					<div className="text-container">
-						<p className="heading-card">XX MOVIE</p>
-						<p className="tagline-card">XX Movie is a website that provides you to find your suitable movies based on the Genres 
-                        and available for any movies from USA, UK, South Korea and Indonesia. We are here to give you an endless solution to bring your movie experience better! <br><br>
-                        All information, data, or any material on this website is the property of XX Cinema and
-                        belong to the official rights holders of the relevant brands, logos, materials.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	);
+const AboutUs = () => {
+    return (
+        <div className="aboutus-container">
+            <div className="aboutus-text-container">
+                <div className="text-group">
+                    <h3>About Us</h3>
+                </div>
+            </div>
+            <div className="aboutus-card-container">
+                <p className="tagline-p">Movie App is a website that provides you to find your suitable movies based on the Genre
+                    and available for any movies from all aroud the world USA, UK, South Korea and Indonesia.
+                    We are here to give you an endless solution to bring your movie experience better.
+                    All information, data, or any material on this website is the property of XX Cinema and belong to the official rights holders of the relevant brands, logos, materials.</p>
+            </div>
+        </div>
+    );
 };
 
 export default AboutUs;
