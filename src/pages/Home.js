1705bc14df52c7204733075ce258f370af55fa0b
hanumaisyaah <hanumaisyaah@gmail.com>
2022-05-30 17:14:05 +0700

    add about us

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../Index.css";
 import Jumbotron from "../Components/Jumbotron/Jumbotron";
 import WhyChooseUs from "../Components/WhyChooseUs/WhyChooseUs";
+import AboutUs from "../Components/AboutUs/AboutUs";
 
 const Home=() => {
 	return (
 		<div className="home-container">
 			<Jumbotron />
 			<WhyChooseUs/>
+			<AboutUs/>
 		</div>
 	);
 };
