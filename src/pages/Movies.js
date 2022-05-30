8dc6eec7430b416496ade40f3c5e72ef49aa93e2
Omar630603 <omar.yem1111@gmail.com>
2022-05-30 16:01:05 +0700

    comment

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,12 +1,13 @@
 import React from "react";
-const Movies=() => {
-	return (
-		<div className="movie-container">
-			<div className="">
-				<h2>Movies</h2>
-			</div>
-		</div>
-	);
+const Movies = () => {
+  return (
+    // change name
+    <div className="movie-container1">
+      <div className="">
+        <h2>Movies</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
