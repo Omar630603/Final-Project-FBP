bb72838145df66b28fc24aec95034b606517f69a
widiarsaf <wretasafitri33@gmail.com>
2022-06-05 13:25:09 +0700

    create login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,7 +4,12 @@ import {
 	useLocation,
 } from "react-router-dom";
 
+// Style
+import "./Style/LoginPage.css";
 
+// Assets
+import Logo from "../Assets/Logo.png";
+import GoogleLogo from "../Assets/Google Logo.png";
 
 const LoginPage=() => {
 	
@@ -30,9 +35,38 @@ const LoginPage=() => {
 		});
 	};
 	return (
-		<div>
-			<p>You must log in to view the page at {from.pathname}</p>
-			<button onClick={login}>Log in</button>
+		<div className="login-container">
+			{/* <p>You must log in to view the page at {from.pathname}</p>
+			<button onClick={login}>Log in</button> */}
+			
+
+			<div>
+				<div className="logo-login-container">
+					<img src={Logo} alt="Logo Movie App" className="logo-image" />
+				</div>
+				<br/>
+				<div className="box-login-container">
+					<h1>Login</h1>
+					<form>
+						<div className="form-group">
+							<label>Email</label>
+							<br/>
+							<input type="email" name="email" id="email-form" placeholder="Enter Your Email"/>
+						</div>
+						<div className="form-group">
+							<label>Password</label>
+							<br />
+							<input type="password" name="password" id="password-form" placeholder="Enter Your Password" />
+						</div>
+						<button className="button-login">Login</button>
+					</form>
+					<hr />
+					<p className="center-text">or</p>
+					<button className="google-button-login"><img src={GoogleLogo} alt="Google Logo" className="google-logo"/>
+						Login With Google</button>
+				</div>
+			</div>
+
 		</div>
 	);
 }
